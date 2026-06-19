1465b862a10562c44fe5dc4151f2f2f46ebef43a
LeoStefL1 <stefanidi.leonardo@gmail.com>
2026-06-19 12:46:12 +0200

    deleted remnants of the old proposal formfrom new_proposal

--- rki_Skript.R
+++ rki_Skript.R
@@ -7,3 +7,10 @@ rki_data <- read_tsv("GBE_Indikatoren_nichtuebertragbarer_Erkrankungen.tsv")
 View(rki_data)
 glimpse(rki_data)
 
+#Number of observations for depressive symptoms (2040201) and 
+#for diagnosed depression (2040202)
+rki_data %>%  filter(Indikator_ID == 2040201) %>% nrow()
+rki_data %>%  filter(Indikator_ID == 2040202) %>% nrow()
+
+rki_data %>% filter(!is.na(Fälle)) %>% nrow()
+
