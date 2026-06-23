345b452076b520b99ad4f12c1c68ab6b9d33a049
LeoStefL1 <stefanidi.leonardo@gmail.com>
2026-06-23 23:21:51 +0200

    I added a proposal for the second research question. I changed the first one, becaus we dont't have continous date of all years ergo we can not pursue the original question

--- rki_Skript.R
+++ rki_Skript.R
@@ -1,5 +1,5 @@
-# This is the exploration of a alternative messier data set 
-
+# This is the exploration of a alternative messier data set
+library(lintr)
 library(readr)
 library(tidyverse)
 
@@ -7,10 +7,12 @@ rki_data <- read_tsv("GBE_Indikatoren_nichtuebertragbarer_Erkrankungen.tsv")
 View(rki_data)
 glimpse(rki_data)
 
-#Number of observations for depressive symptoms (2040201) and 
+#Number of observations for depressive symptoms (2040201) and
 #for diagnosed depression (2040202)
-rki_data %>%  filter(Indikator_ID == 2040201) %>% nrow()
-rki_data %>%  filter(Indikator_ID == 2040202) %>% nrow()
+rki_data |> filter(Indikator_ID == 2040201) |> nrow()
+rki_data |> filter(Indikator_ID == 2040202) |> nrow()
 
-rki_data %>% filter(!is.na(Fälle)) %>% nrow()
+rki_data |> filter(!is.na(Fälle)) |> nrow()
+
+lint(filename = "rki_Skript.R")
 
